29ca1fa566e7d1c7365ff1020d0c438172845bce
Jerry <jerry8812@hotmail.com>
2021-09-12 14:41:58 +1200

    Updates

--- static/js/main.d9713839.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpe-website-template"]=this["webpackJsonpe-website-template"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),s=n.n(c),i=n(3),r=n.n(i),a=(n(8),n(0));var o=function(){return Object(a.jsx)("div",{className:"App",children:"Hello"})};r.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.d9713839.chunk.js.map
